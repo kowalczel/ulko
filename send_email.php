cd974f0921592bb477c3ff35c27fa74be4084062
kowalczel <kowalcze.l@gmail.com>
2024-12-21 20:35:46 +0100

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Pobranie danych z formularza
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Adres, na który zostanie wysłany e-mail
+    $to = "twoj-adres-email@example.com";
+
+    // Temat e-maila
+    $subject = "Zapytanie ze strony internetowej";
+
+    // Treść e-maila
+    $body = "Imię i nazwisko: $name\n";
+    $body .= "E-mail: $email\n";
+    $body .= "Wiadomość:\n$message\n";
+
+    // Nagłówki e-maila
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Wysyłanie wiadomości
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Wiadomość została wysłana.";
+    } else {
+        echo "Wystąpił problem podczas wysyłania wiadomości.";
+    }
+} else {
+    echo "Nieprawidłowe żądanie.";
+}
+?>
